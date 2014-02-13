321760b6100374428c0424ec23477c75fdd1b7ad
TF Nelson <tails.fanatic@gmail.com>
2014-02-13 06:15:05 -0600

    Finished menu render code; started menu data structure assignment code

--- touhou.py
+++ touhou.py
@@ -37,11 +37,23 @@ class TouhouLauncher(object):
 		self.menu_position = 0
 		self.init_menus(menu_data)
 		self.set_menu("main")
+		self.config = configuration
 
 	def init_menus(self, data):
 		self._menus = dict()
 		for menu in data:
 			self._menus[menu['id']] = menu
+			
+		targetstack = list()
+		targetstack.append(self._menus)
+		
+		for menu in targetstack[-1]:
+			for subitem in 
+			
+	def assign_menu_subitems(self, menu):
+		ids = list()
+		for itemid in menu['items']:
+			ids.append(itemid)
 
 	def set_menu(self, id):
 		self._current_menu = self._menus[id]
@@ -93,19 +105,58 @@ class TouhouLauncher(object):
 
 	def draw_items(self):
 		coords = self.draw_selected_item()
-		self.draw_upper_items(coords)
-		self.draw_lower_items(coords)
+		self.draw_other_items(coords, above=True)
+		self.draw_other_items(coords, above=False)
 		
 	def draw_selected_item(self):
 		items = self.get_menu_items()
 		item = items[self.menu_position]
-		self.blit_text(
-
-	def draw_upper_items(self):
+		text = self.make_text_surface(item['title'], size=self.config['menu_item_font_size_selected'])
+		textr = text.get_rect()
+		x, y = self.get_selected_coords(textr)
+		self.blit_surface(text, x, y)
+		return textr
+		
+	def get_selected_coords(self, selrect):
+		x = round((self.config['menu_percent_width']/100) * self.get_width())
+		y = round(((self.config['menu_main_item_percent_height']/100) * self.get_height()) - (selrect.height / 2))
+		return (x, y)
+				
+	def draw_other_items(self, selrect, above=True):
+		dirmult = 1
+		if above:
+			dirmult *= -1
 		items = self.get_menu_items()
-		pass
+		num = 1
+		while True:
+			pos = self.menu_position + (mult * num)
+			if pos < 0 or pos >= len(items):
+				break
+			item = items[pos]
+			text = self.make_text_surface(item['title'], size=self.config['menu_item_font_size'])
+			textr = text.get_rect()
+			ydiff = self.config['menu_item_vertical_spacing']
+			if above or num is not 1:
+				ydiff += textr.height
+			else:
+				ydiff += selrect.height
+			x = selrect.x
+			y = selrect.y + (ydiff * num)
+			if y < 0 or y + textr > self.get_height():
+				break
+			else:
+				self.blit_surface(text, x, y)
+				num += 1
+				
+	def get_height(self):
+		info = pygame.display.Info()
+		return info['current_h']
+		
+	def get_width(self):
+		info = pygame.display.Info()
+		return info['current_w']
 
-	def get_menu_items(self):
+	def get_menu_items(self, selrect):
 		return self._current_menu['items']
 
 	def start_movie(self):
@@ -139,11 +190,13 @@ class TouhouLauncher(object):
 		msg = str(remain) + " seconds until video..."
 		self.write_message(msg)
 
-	def blit_text(self, msg, x, y, font=pygame.font.get_default_font(), color=COLOR_WHITE, size=12, aa=True):
-		surface = pygame.font.Font(font, size).render(msg, aa, color)
-		msgrect = msgSurface.get_rect()
-		msgrect.topleft = (x, y)
-		self.window_surface.blit(surface, msgrect)
+	def make_text_surface(self, msg, font=pygame.font.get_default_font(), color=COLOR_WHITE, size=12, aa=True):
+		return pygame.font.Font(font, size).render(msg, aa, color)
+		
+	def blit_surface(self, surface, x, y):
+		srect = surface.get_rect()
+		srect.topleft = (x, y)
+		self.window_surface.blit(surface, srect)
 
 	def write_message(self, msg):
 		msgSurface = self.msgfont.render(msg, False, COLOR_WHITE)
