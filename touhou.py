a0efba4310fae79ba5ce8d44946c8f6b0ad70a66
TF Nelson <tails.fanatic@gmail.com>
2014-02-13 10:29:26 -0600

    Fixed menu alignment and added menu moving

--- touhou.py
+++ touhou.py
@@ -34,7 +34,7 @@ class TouhouLauncher(object):
 		self.videos = video_list
 		self.videos_position = 0
 		self.idle_timeout = configuration['idle_timeout']
-		self.menu_position = 0
+		self._menu_position = 0
 		self.init_menus(menu_data)
 		self.set_menu(self.config['root_menu_id'])
 		self.background_surf = pygame.image.load(config['background'])
@@ -62,6 +62,14 @@ class TouhouLauncher(object):
 	def set_menu(self, id):
 		self._current_menu = self._menus[id]
 
+	def select_next_item(self):
+		if (self._menu_position < len(self._current_menu)):
+			self._menu_position += 1
+
+	def select_prev_item(self):
+		if (self._menu_position > 0):
+			self._menu_position -= 1
+
 	def start(self):
 		while self.running:
 			self.update()
@@ -77,6 +85,10 @@ class TouhouLauncher(object):
 				self.exit_movie_mode()
 				if event.key == K_ESCAPE:
 					pygame.event.post(pygame.event.Event(QUIT))
+				elif event.key == K_UP:
+					self.select_prev_item()
+				elif event.key == K_DOWN:
+					self.select_next_item()
 			elif event.type == QUIT:
 			# check quit last so exit is not followed by pygame calls
 				self.exit()
@@ -114,7 +126,7 @@ class TouhouLauncher(object):
 		
 	def draw_selected_item(self):
 		items = self.get_menu_items()
-		item = items[self.menu_position]
+		item = items[self._menu_position]
 		text = self.make_text_surface(item['title'], size=self.config['menu_item_font_size_selected'])
 		textr = text.get_rect()
 		x, y = self.get_selected_coords(textr)
@@ -133,19 +145,19 @@ class TouhouLauncher(object):
 		items = self.get_menu_items()
 		num = 1
 		while True:
-			pos = self.menu_position + (direction_mult * num)
+			pos = self._menu_position + (direction_mult * num)
 			if pos < 0 or pos >= len(items):
 				break
 			item = items[pos]
 			text = self.make_text_surface(item['title'], size=self.config['menu_item_font_size'])
 			textr = text.get_rect()
-			ydiff = self.config['menu_item_vertical_spacing']
-			if above or num is not 1:
-				ydiff += textr.height
+			ydiff = self.config['menu_item_vertical_spacing'] + textr.height
+			if above:
+				offset = ydiff
 			else:
-				ydiff += selrect.height
+				offset = selrect.height + textr.height
 			x = selrect.x
-			y = selrect.y + (ydiff * direction_mult * num)
+			y = selrect.y + (offset + (ydiff * (num-1))) * direction_mult
 			if y < 0 or y + textr.height > self.get_height():
 				break
 			else:
